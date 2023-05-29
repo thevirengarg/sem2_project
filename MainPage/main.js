6e988b8be84fe837b3fee96c1528d0ae1017fac5
Somya Bhatt <somyabhattcu1@gmail.com>
2023-05-29 14:46:57 +0530

    updated

--- MainPage/main.js
+++ MainPage/main.js
@@ -5,15 +5,22 @@ let spaceCount = document.getElementById("space-count");
 let paragraph = document.getElementById("para-count");
 let button = document.getElementById("clr");
 let sentence = document.getElementById("sentence-count");
+let readingtime = document.getElementById("reading");
+let speaking = document.getElementById("speaking");
 
 inputTextArea.addEventListener("input", function(){
+  let txt = inputTextArea.value.trim();
+  let countingword = txt.split(/\s+/).filter((item) => item).length;
+  let read = Math.floor(countingword * (60/275));
+  let speak = Math.floor(countingword *(60/180));
 characCount.textContent = (inputTextArea.value.match(/[a-z0-9.]/gi) || []).length;
 // g - global , i - capital also
-   let txt = inputTextArea.value.trim();
-  wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+  wordCount.textContent = countingword;
   spaceCount.textContent = inputTextArea.value.split(" ").length -1;
   paragraph.textContent = inputTextArea.value.split("\n").filter((item) => item).length;
   sentence.textContent = inputTextArea.value.split(".").length -1;
+  readingtime.textContent = read;
+  speaking.textContent = speak;
 });
 
 
@@ -25,6 +32,8 @@ button.addEventListener("click", function(){
   paragraph.textContent = 0;
   characCount.textContent = 0;
   sentence.textContent = 0;
+  readingtime.textContent = 0;
+  speaking.textContent = 0;
 });
 
 
